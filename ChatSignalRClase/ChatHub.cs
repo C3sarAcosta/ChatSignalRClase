37c5da9c00b5c1855fd33c45f52aefe41e972690
C3sarAcosta <c3sar.adan@gmail.com>
2022-11-07 09:55:51 -0600

    Agregar archivos de proyecto.

---
+++ ChatSignalRClase/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatSignalRClase
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task SenMessage(int room, string user, string message)
+        {
+            //Enviamos un mensaje de forma asincorna a un grupo determinado
+            await Clients.Group(room.ToString()).SendAsync("RecibirMensaje", user, message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            //Añadimos a la persona a la sala
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+        }
+    }
+}
---
+++ ChatSignalRClase/Controllers/ChatsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSignalRClase.Controllers
+{
+    public class ChatsController : Controller
+    {
+        public static Dictionary<int, string> ListaChats = new Dictionary<int, string>()
+        {
+            { 1, "Contabilidad"},
+            { 2, "Programacion"},
+            { 3, "Pasar tareas"},
+            { 4, "Pasar examenes"}
+        };
+
+        public IActionResult ListaDeChats()
+        {
+            return View(ListaChats);
+        }
+
+        public IActionResult Chat(int idChat)
+        {
+            return View("Chat", idChat);
+        }
+    }
+}
